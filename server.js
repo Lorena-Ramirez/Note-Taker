18e78a5c2c92c24b75148cf2fe720ec63a5bd096
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-12-05 13:36:05 -0500

    Set upp basic server adding dependencies , port, json handler, require routes and app.listen

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+//Set up dependencies
 const express = require("express");
+
+//Set up basic properties for express server
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log("you've connected at http://localhost:" + PORT);
-});
+// Let's express handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Point server to route files
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
