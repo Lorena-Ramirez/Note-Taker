441641eaa9a25561f0a3a5e21d0ad452ef307905
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-12-05 17:39:56 -0500

    used fs to write file containing new JSON data

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,15 +10,27 @@ module.exports = function(app) {
     res.json(savedNotes);
   });
 
+  //Adding a new note
   app.post("/api/notes", function(req, res) {
     var newNote = req.body;
 
     //assign an Id to each new note
     newNote.id = savedNotes.length.toString();
     
-    //push new note into db SON file
+    //push new note into db JSON file
     savedNotes.push(newNote);
-  
+
+    // Write a JSON file so update with new data
+    fs.writeFile("./db/db.json", JSON.stringify(savedNotes),(err) => {
+      if (err) {
+        console.log(err); 
+        res.sendStatus(404);
+      } 
+      else { 
+          console.log("Success!");
+          res.sendStatus(200);
+      }
+    });  
   });
 
   
