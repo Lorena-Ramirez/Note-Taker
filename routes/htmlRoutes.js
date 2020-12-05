e6b5bd01ca85c99a8dad29c52767b01575d371ca
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-12-05 14:48:38 -0500

    Set routes for notes and index.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,14 @@
 //Include path package 
-const { builtinModules } = require("module");
 var path = require("path");
 
-module.exports = function (app) {
+module.exports = function(app) {
 //Set a path for notes.    
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
   //Set catch all path that leads to index.html
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/tables.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
