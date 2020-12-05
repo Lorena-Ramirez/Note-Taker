acb660ed1136333a0e65c9593c77e835dc70d105
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-12-05 17:23:51 -0500

    added static file

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
+
+
 //Point server to route files
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
