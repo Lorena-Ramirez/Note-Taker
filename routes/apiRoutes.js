4082ad3f9eea53663746242525b80b27e2eff1a3
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2020-12-05 15:20:21 -0500

    Added a post that pushes a newnote into the db json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,9 +2,19 @@
 var savedNotes = require("../db/db");
 module.exports = function(app) {
 
+  //return saved notes as JSON
   app.get("/api/notes", function(req, res) {
     res.json(savedNotes);
   });
+
+  app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    savedNotes.push(newNote);
+    console.log(newNote);
+  });
+
+  
+
 
     
 };
